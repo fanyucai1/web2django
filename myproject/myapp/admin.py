eed3945e1444497792e2103e56756fbd36f2f70d
fanyucai1 <fanyucai1@126.com>
2024-04-22 15:58:30 +0800

    no

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -4,8 +4,8 @@ from django.contrib import admin
 from .models import task
 
 class taskModelAdmin(admin.ModelAdmin):
-    list_display = ('pub_date','user_test')
-    search_fields = ('user_test',)
+    list_display = ('no','pub_date','user_test')
+    search_fields = ('user_test','pub_date')
     ordering = ('user_test',)
 
 
--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class task(models.Model):
+    number = models.AutoField(primary_key=True, verbose_name='编号')
     pub_date=models.DateTimeField(verbose_name="时间")
     user_test=models.CharField(verbose_name="用户名",max_length=200)
     class Meta:
